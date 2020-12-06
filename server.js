0fa41ebc6b21fdd3ec3faa4d3c6b8493ac8db955
fischer <pedro.fischer22@gmail.com>
2020-12-06 16:32:32 -0300

    [add] configuraçao para deploy

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-//Install express server
 const express = require('express');
-const path = require('path');
-
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/devforum'));
+const PORT = process.env.PORT || 8081; 
 
-app.get('*', (req, res) => {
-    res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+app.use(express.static(__dirname + '/dist/front')); 
+
+app.get('/*', (req, res) => {
+    res.sendFile(__dirname + '/dist/front/index.html');
+})
+
+app.listen(PORT, () => {
+    console.log('server working on: ' + PORT);
 });
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8081);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://apiids.herokuapp.com/api',
+  clientUrl: 'https://apiids.herokuapp.com',
+  localstorage: {
+    tokenName: 'scrtoken'
+  }
 };
