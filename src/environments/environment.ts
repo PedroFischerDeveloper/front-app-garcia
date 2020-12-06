5eefb0de604ac6d7e8e72e399da06eb9168d360f
fischer <pedro.fischer22@gmail.com>
2020-12-06 16:46:42 -0300

    add

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://apiids.herokuapp.com/api',
+  apiUrl: 'http://apiids.herokuapp.com/api',
   clientUrl: 'http://localhost:4200',
   localstorage: {
     tokenName: 'scrtoken'
